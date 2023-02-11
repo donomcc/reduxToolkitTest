51300eae5569363f99356ad78dffb52608ff5418
Donovan Mccollum <donomcc@gmail.com>
2023-02-11 16:32:12 -0600

    Create store and counter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div>
+      <div>
+        <h1>Count: </h1>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
