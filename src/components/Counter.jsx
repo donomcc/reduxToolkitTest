c067451715c5c588b538d1e7b159569aafd3e5fb
Donovan Mccollum <donomcc@gmail.com>
2023-02-11 17:10:32 -0600

    Added CSS and also added incrementByAmount functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../slices/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../slices/counterSlice";
 
 function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+  const [incrementAmount, setIncrementAmount] = useState("1");
 
   return (
     <div>
-      <div>
+      <div className="counter">
         <h1>Count: {count}</h1>
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <div>
+          <label>Increase Amount: </label>
+          <input
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+          />
+          <button
+            onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+          >
+            Add Amount
+          </button>
+        </div>
       </div>
     </div>
   );
